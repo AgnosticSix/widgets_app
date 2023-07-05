5784e76e90146d602f4b13bb377ce869fbc01e8f
AgnosticSix <mateo_godfather@hotmail.com>
2023-07-05 17:10:19 -0600

    apptheme and menuitems created

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String url;
+  final IconData icon;
+
+  const MenuItems(
+      {required this.title,
+      required this.subTitle,
+      required this.url,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    url: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItems(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    url: '/card',
+    icon: Icons.credit_card,
+  )
+];
